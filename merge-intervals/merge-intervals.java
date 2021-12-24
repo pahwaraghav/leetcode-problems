1dbfa8e445559d43f998ac9703909f9da8b4d801
Raghav Pahwa <72634414+pahwaraghav@users.noreply.github.com>
2021-12-24 18:29:34 +0530

    Time: 6 ms (57.88%), Space: 41.1 MB (98.99%) - LeetHub

--- merge-intervals/merge-intervals.java
+++ merge-intervals/merge-intervals.java
@@ -1,20 +1,20 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
-        ArrayList<int[]> mergedArrayList = new ArrayList<int[]>();
-        mergedArrayList.add(intervals[0]);
+       Arrays.sort(intervals,(a,b) -> a[0] - b[0]);
+        List<int[]> arr = new ArrayList<int[]>();
+        arr.add(intervals[0]);
         for(int i=1; i<intervals.length;i++){
-            int[] interval = mergedArrayList.get(mergedArrayList.size()-1);
-            if(intervals[i][0] <= interval[1]){
+            int[] interval = arr.get(arr.size()-1);
+            if(interval[1] >= intervals[i][0]){
                 interval[1] = Math.max(interval[1],intervals[i][1]);
             }
             else{
-                mergedArrayList.add(intervals[i]);
+                arr.add(intervals[i]);
             }
         }
-        int[][] res = new int[mergedArrayList.size()][2];
-        for(int i=0; i<mergedArrayList.size();i++){
-            res[i] = mergedArrayList.get(i);
+        int[][] res = new int[arr.size()][2];
+        for(int i=0; i<arr.size();i++){
+            res[i] = arr.get(i);
         }
         return res;
     }

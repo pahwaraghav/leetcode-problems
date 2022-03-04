599d343b0b5a733a6a9aebb49afb0724b0a9a997
Raghav Pahwa <72634414+pahwaraghav@users.noreply.github.com>
2022-03-04 21:28:23 +0530

    Time: 4 ms (90.87%), Space: 42.9 MB (66.21%) - LeetHub

---
+++ 799-champagne-tower/799-champagne-tower.java
@@ -0,0 +1,17 @@
+class Solution {
+    public double champagneTower(int poured, int query_row, int query_glass) {
+        double[][] dp = new double[102][102];
+        dp[0][0] = (double) poured;
+        
+        for(int i = 0; i<query_row; i++){
+            for(int j=0; j<= i; j++){
+                double k = (dp[i][j] - 1.0) / 2.0;
+                if(k > 0){
+                    dp[i+1][j] += k;
+                    dp[i+1][j+1] += k;
+                }
+            }
+        }
+        return Math.min(1,dp[query_row][query_glass]);
+    }
+}

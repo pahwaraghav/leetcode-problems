3e44bea8479c0c8f673ccb62a5a97ab21b2ac4d6
Raghav Pahwa <72634414+pahwaraghav@users.noreply.github.com>
2022-03-28 02:22:33 +0530

    Time: 25 ms (47.22%), Space: 60.5 MB (38.00%) - LeetHub

--- 930-binary-subarrays-with-sum/930-binary-subarrays-with-sum.java
+++ 930-binary-subarrays-with-sum/930-binary-subarrays-with-sum.java
@@ -3,13 +3,12 @@ class Solution {
         HashMap<Integer,Integer> hmap = new HashMap<Integer,Integer>();
         hmap.put(0,1);
         int res = 0;
+        int sum = 0;
         for(int i=0; i<nums.length;i++){
-            if(i != 0){
-                nums[i] += nums[i-1];
-            }
-            int num = hmap.getOrDefault(nums[i] - goal,0);
+            sum += nums[i];
+            int num = hmap.getOrDefault(sum - goal,0);
             res += num;
-            hmap.put(nums[i],hmap.getOrDefault(nums[i],0)+1);
+            hmap.put(sum,hmap.getOrDefault(sum,0)+1);
         }
         return res;
     }

1752fcfe5749a8b5516e10ecdb92680d0cb8008c
Raghav Pahwa <72634414+pahwaraghav@users.noreply.github.com>
2022-03-27 18:02:32 +0530

    Time: 8 ms (96.31%), Space: 50.7 MB (97.60%) - LeetHub

--- 848-shifting-letters/848-shifting-letters.java
+++ 848-shifting-letters/848-shifting-letters.java
@@ -1,15 +1,16 @@
 class Solution {
     public String shiftingLetters(String s, int[] shifts) {
-        for(int i=shifts.length-2; i>=0;i--){
-            shifts[i] = (shifts[i] + shifts[i+1])%26;
-        }
         char[] chars = s.toCharArray();
-        for(int i=0; i<chars.length;i++){
+        for(int i=shifts.length-1; i>=0;i--){
+            if(i!=shifts.length-1){
+                shifts[i] = (shifts[i] + shifts[i+1])%26; 
+            }
             int n = chars[i] - 'a';
             n = (n + shifts[i]) %26;
             char ch = (char) (n + 'a');
             chars[i] = ch;
         }
+
         return new String(chars);
     }
 }

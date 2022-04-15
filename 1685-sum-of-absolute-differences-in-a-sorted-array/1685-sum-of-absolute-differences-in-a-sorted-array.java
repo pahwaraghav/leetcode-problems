69c47b33c78e78c5624102dea0926d7ca9c8a82d
Raghav Pahwa <72634414+pahwaraghav@users.noreply.github.com>
2022-04-15 16:58:17 +0530

    Time: 5 ms (68.15%), Space: 55.1 MB (96.37%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.java
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.java
@@ -1,15 +1,6 @@
 class Solution {
     public int[] getSumAbsoluteDifferences(int[] nums) {
-        int[] preSum = new int[nums.length];
         int[] postSum = new int[nums.length];
-        for(int i=0; i<nums.length;i++){
-            if(i==0){
-                preSum[i] = nums[i];
-            }
-            else{
-                preSum[i] = nums[i] + preSum[i-1];
-            }
-        }
         for(int i=nums.length-1; i>=0;i--){
             if(i == nums.length-1){
                 postSum[i] = nums[i];
@@ -19,14 +10,16 @@ class Solution {
             }
         }
         int[] res = new int[nums.length];
+        int pSum = 0;
         for(int i=0; i<res.length;i++){
             int lNum = 0, rNum = 0;
             if(i!=0){
-                lNum = i * nums[i] - preSum[i-1];
+                lNum = i * nums[i] - pSum;
             }
             if(i != res.length -1){
                 rNum = postSum[i+1] - (res.length-1-i) * nums[i];
             }
+            pSum += nums[i];
             res[i] = lNum + rNum;
         }
         return res;

d99d634b2516eb51a678c62f0246127bae0fe453
Raghav Pahwa <72634414+pahwaraghav@users.noreply.github.com>
2022-04-23 16:18:54 +0530

    Time: 5 ms (12.88%), Space: 44.5 MB (67.34%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.java
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -1,27 +1,13 @@
-class Pair{
-    int min;
-    int max;
-    Pair(){}
-    Pair(int min, int max){
-        this.min = min;
-        this.max = max;
-    }
-}
 class Solution {
     public int maxProduct(int[] nums) {
         int res = nums[0];
-        Pair[] arr = new Pair[nums.length];
-        for(int i=0; i<nums.length;i++){
-            if(i==0){
-                arr[i] = new Pair(nums[i],nums[i]);
-            }
-            else{
-                Pair p = new Pair();
-                p.min = Math.min(nums[i], Math.min(arr[i-1].min*nums[i],arr[i-1].max*nums[i]));
-                p.max = Math.max(nums[i], Math.max(arr[i-1].min*nums[i],arr[i-1].max*nums[i]));
-                arr[i] = p;
-            }
-            res = Math.max(res,Math.max(arr[i].min,arr[i].max));
+        int cur_min = nums[0];
+        int cur_max = nums[0];
+        for(int i=1; i<nums.length;i++){
+            int temp = cur_min;
+            cur_min = Math.min(nums[i], Math.min(cur_min*nums[i], cur_max*nums[i]));
+            cur_max = Math.max(nums[i], Math.max(temp*nums[i],cur_max*nums[i]));       
+            res = Math.max(res,Math.max(cur_min,cur_max));
         }
         return res;
     }

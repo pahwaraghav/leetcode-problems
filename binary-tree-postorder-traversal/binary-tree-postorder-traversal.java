b952997af0e2052d8e54d081869db4e04c29c0c7
Raghav Pahwa <72634414+pahwaraghav@users.noreply.github.com>
2022-04-24 19:14:10 +0530

    Time: 1 ms, Memory: 42.4 MB - LeetHub

--- binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -16,13 +16,26 @@
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> lis = new ArrayList<>();
-        postOrder(root,lis);
+        if(root == null) return lis;
+        HashSet<TreeNode> visited = new HashSet<>();
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
+        while(!st.isEmpty()){
+            TreeNode curr = st.peek();
+            if(curr.left == null || visited.contains(curr.left)){
+                if(curr.right == null || visited.contains(curr.right)){
+                    st.pop();
+                    lis.add(curr.val);
+                    visited.add(curr);
+                }
+                else{
+                    if(curr.right!=null) st.push(curr.right);
+                }
+            }
+            else{
+                if(curr.left !=null) st.push(curr.left);
+            }
+        }
         return lis;
-    }
-    public void postOrder(TreeNode root, List<Integer> lis){
-        if(root == null) return;
-        postOrder(root.left,lis);
-        postOrder(root.right,lis);
-        lis.add(root.val);
     }
 }

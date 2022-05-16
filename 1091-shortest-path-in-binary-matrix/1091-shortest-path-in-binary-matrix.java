5a5613c0d7991803afba414803807a6d6564aa6c
Raghav Pahwa <72634414+pahwaraghav@users.noreply.github.com>
2022-05-16 10:52:53 +0530

    Time: 45 ms (22.71%), Space: 72.7 MB (12.67%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        if(grid[0][0] != 0 || grid[m-1][n-1] != 0){
+            return -1;
+        }
+        boolean[][] visited = new boolean[m][n];
+        visited[0][0] = true;
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{0,0});
+        int ans = 1;
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i=0; i<size;i++){
+                int[] cur = q.poll();
+                if(cur[0] == m-1 && cur[1] == n-1){
+                    return ans;
+                }
+                for(int j=-1; j<2;j++){
+                    for(int k=-1;k<2;k++){
+                        //if(j==0 && k==0) continue;
+                        int newX = cur[0] + j;
+                        int newY = cur[1] + k;
+                        int[] arr = new int[]{newX,newY};
+                        if(isValid(grid,newX,newY) && grid[newX][newY] == 0 && !visited[newX][newY]){
+                            visited[newX][newY] = true;
+                            q.offer(arr);
+                        }
+                    }
+                }
+            }
+            ans++;
+        }
+        return -1;
+    }
+    public boolean isValid(int[][] grid, int i,int j){
+        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length) return false;
+        return true;
+    }
+}

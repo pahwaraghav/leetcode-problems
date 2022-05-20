14104408da452184ebeb65c604a860770171e41e
Raghav Pahwa <72634414+pahwaraghav@users.noreply.github.com>
2022-05-20 10:42:04 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (95.62%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int num = m + n -2;
+        int r = m-1;
+        double res = 1;
+        for(int i=1; i<=r; i++){
+            res = res * (num - r + i) /i;
+        }
+        return (int) res;
+    }
+}

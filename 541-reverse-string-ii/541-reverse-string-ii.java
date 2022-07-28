c111a853e4daf33bf5c9bb85912b309ce90d00f3
Raghav Pahwa <72634414+pahwaraghav@users.noreply.github.com>
2022-07-28 19:48:47 +0530

    Time: 11 ms (12.63%), Space: 47 MB (9.68%) - LeetHub

---
+++ 541-reverse-string-ii/541-reverse-string-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String reverseStr(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+        int i=0;
+        boolean flag = true;
+        while(i<s.length()){
+            if(!flag){
+                int end = Math.min(i+k, s.length());
+                sb.append(s.substring(i,end));
+                i+=k;
+            }
+            else{
+                int j = Math.min(s.length() -1, i+k-1);
+                for(; j>=i; j--){
+                    sb.append(s.charAt(j)+"");
+                }
+                i+=k;
+            }
+            flag = !flag;
+        }
+        return sb.toString();
+    }
+}

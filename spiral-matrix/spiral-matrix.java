c673cde869b1e6c23070377ef18df80896226134
Raghav Pahwa <72634414+pahwaraghav@users.noreply.github.com>
2023-05-09 09:57:00 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (31.34%) - LeetSync

--- spiral-matrix/spiral-matrix.java
+++ spiral-matrix/spiral-matrix.java
@@ -1,54 +1,38 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        int top = 0;
-        int bottom = matrix.length-1;
         int left = 0;
         int right = matrix[0].length-1;
-        
-        int totalSize = matrix.length * matrix[0].length;
-        int count = 0;
-        
-        List<Integer> res = new ArrayList<>();
-        
-        while(top<=bottom && left<=right){
-            for(int i=left; i<=right;i++){
-                if(count>=totalSize){
-                    break;
+        int top = 0;
+        int bottom = matrix.length-1;
+        int total = matrix.length * matrix[0].length;
+        int curr = 0;
+        List<Integer> ans = new ArrayList<>();
+        while(curr<total){
+            if(curr<total && top<=bottom){
+                for(int i=left; i<=right; i++){
+                    ans.add(matrix[top][i]); curr++;
                 }
-                res.add(matrix[top][i]);
-                count++;
-                
             }
             top++;
-            
-            for(int i=top;i<=bottom;i++){
-                if(count>=totalSize){
-                    break;
+            if(curr<total && left<=right){
+                for(int i=top; i<=bottom; i++){
+                    ans.add(matrix[i][right]); curr++;
                 }
-                res.add(matrix[i][right]);
-                count++;
             }
             right--;
-            
-            for(int i=right;i>=left;i--){
-                if(count>=totalSize){
-                    break;
+            if(curr<total && bottom>=top){
+                for(int i=right; i>=left; i--){
+                    ans.add(matrix[bottom][i]); curr++;
                 }
-                res.add(matrix[bottom][i]);
-                count++;
             }
             bottom--;
-            
-            for(int i=bottom;i>=top;i--){
-                if(count>=totalSize){
-                    break;
+            if(curr<total && right>= left){
+                for(int i=bottom; i>=top; i--){
+                    ans.add(matrix[i][left]); curr++;
                 }
-                res.add(matrix[i][left]);
-                count++;
             }
             left++;
         }
-        
-        return res;
+        return ans;
     }
 }

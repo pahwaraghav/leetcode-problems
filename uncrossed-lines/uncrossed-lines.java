ff765e937a3b3f4c2958f35a69b52fa879df277f
Raghav Pahwa <72634414+pahwaraghav@users.noreply.github.com>
2023-05-12 03:10:27 +0530

    Time: 5 ms (92.49%) | Memory: 42.1 MB (67.80%) - LeetSync

--- uncrossed-lines/uncrossed-lines.java
+++ uncrossed-lines/uncrossed-lines.java
@@ -1,20 +1,16 @@
 class Solution {
     public int maxUncrossedLines(int[] nums1, int[] nums2) {
-        int[][] dp = new int[nums1.length][nums2.length];
-        for(int[] arr: dp){
-            Arrays.fill(arr,-1);
+        int[][] dp = new int[nums1.length+1][nums2.length+1];
+        for(int i=0; i<nums1.length; i++){
+            for(int j=0; j<nums2.length; j++){
+                if(nums1[i] == nums2[j]){
+                    dp[i+1][j+1] = 1 + dp[i][j];
+                }
+                else{
+                    dp[i+1][j+1] = Math.max(dp[i+1][j], dp[i][j+1]);
+                }
+            }
         }
-        return helper(nums1, nums2, nums1.length-1, nums2.length-1, dp);
-    }
-    public int helper(int[] nums1, int[] nums2, int i, int j, int[][] dp){
-        if(i<0 || j<0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(nums1[i] == nums2[j]){
-            dp[i][j] = 1 + helper(nums1, nums2, i-1, j-1, dp);
-        }
-        else{
-            dp[i][j] = Math.max(helper(nums1, nums2, i-1, j, dp), helper(nums1, nums2, i, j-1, dp));
-        }
-        return dp[i][j];
+        return dp[nums1.length][nums2.length];
     }
 }

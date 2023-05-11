500e2ba8158f086c34bc3ad492ca7b0f6e41667c
Raghav Pahwa <72634414+pahwaraghav@users.noreply.github.com>
2023-05-12 03:07:00 +0530

    Time: 9 ms (16.22%) | Memory: 42.3 MB (41.65%) - LeetSync

--- uncrossed-lines/uncrossed-lines.java
+++ uncrossed-lines/uncrossed-lines.java
@@ -4,16 +4,16 @@ class Solution {
         for(int[] arr: dp){
             Arrays.fill(arr,-1);
         }
-        return helper(nums1, nums2, 0, 0, dp);
+        return helper(nums1, nums2, nums1.length-1, nums2.length-1, dp);
     }
     public int helper(int[] nums1, int[] nums2, int i, int j, int[][] dp){
-        if(i>=nums1.length || j>=nums2.length) return 0;
+        if(i<0 || j<0) return 0;
         if(dp[i][j] != -1) return dp[i][j];
         if(nums1[i] == nums2[j]){
-            dp[i][j] = 1 + helper(nums1, nums2, i+1, j+1, dp);
+            dp[i][j] = 1 + helper(nums1, nums2, i-1, j-1, dp);
         }
         else{
-            dp[i][j] = Math.max(helper(nums1, nums2, i+1, j, dp), helper(nums1, nums2, i, j+1, dp));
+            dp[i][j] = Math.max(helper(nums1, nums2, i-1, j, dp), helper(nums1, nums2, i, j-1, dp));
         }
         return dp[i][j];
     }

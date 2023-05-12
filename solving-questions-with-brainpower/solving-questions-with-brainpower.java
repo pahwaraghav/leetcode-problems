e779e586c59e249907568513f48375e4f2035889
Raghav Pahwa <72634414+pahwaraghav@users.noreply.github.com>
2023-05-12 21:56:03 +0530

    Time: 20 ms (37.04%) | Memory: 107.3 MB (50.26%) - LeetSync

--- solving-questions-with-brainpower/solving-questions-with-brainpower.java
+++ solving-questions-with-brainpower/solving-questions-with-brainpower.java
@@ -1,16 +1,14 @@
 class Solution {
     public long mostPoints(int[][] questions) {
-        long maxVal = 0;
         long[] dp = new long[questions.length];
-        for(int i=questions.length -1; i >=0 ;i--){
-            long nextScore = 0;
-            if(i + questions[i][1] + 1 < questions.length){
-                nextScore = dp[i + questions[i][1] + 1];
-            }
-            dp[i] = Math.max(maxVal, questions[i][0] + nextScore);
-           
-            maxVal = Math.max(maxVal, dp[i]);
-        }
-        return dp[0];
+        Arrays.fill(dp,-1);
+        return helper(questions, dp, 0);
+    }
+    public long helper(int[][] questions, long[] dp, int i){
+        if(i>= questions.length) return 0;
+        if(dp[i] != -1) return dp[i];
+        long solve = questions[i][0] + helper(questions, dp, i+questions[i][1] + 1);
+        long notSolve = helper(questions, dp, i+1);
+        return dp[i] = Math.max(solve, notSolve);
     }
 }

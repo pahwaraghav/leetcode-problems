ecae6db5b81ae8d7df2d3cddeb3d2f8ad28530a5
Raghav Pahwa <72634414+pahwaraghav@users.noreply.github.com>
2023-07-06 22:51:49 +0530

    Time: 38 ms (5.12%) | Memory: 54.3 MB (58.85%) - LeetSync

--- minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -1,27 +1,24 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int[] prefix = new int[nums.length+1];
-        prefix[0] = 0;
-        for(int i=0; i<nums.length; i++){
-            if(i==0) prefix[i+1] = nums[i];
-            else prefix[i+1] = prefix[i] + nums[i];
-        }
+        ArrayList<Integer> prefix = new ArrayList<>();
+        prefix.add(0);
         int ans = nums.length+1;
-        for(int i=0; i<prefix.length; i++) {
-            int val = prefix[i] - target;
+        for(int i=0; i<=nums.length; i++) {
+            if(i!= nums.length) prefix.add(prefix.get(prefix.size()-1) + nums[i]);
+            int val = prefix.get(i) - target;
             int idx = lowerBound(prefix, val);
             if(idx == -1) continue;
             ans = Math.min(ans, i - idx);
         }
         return ans == nums.length + 1 ? 0 : ans;
     }
-    public int lowerBound(int[] arr, int target) {
+    public int lowerBound(ArrayList<Integer> arr, int target) {
         int low = 0;
-        int high = arr.length-1;
+        int high = arr.size()-1;
         int ans = -1;
         while(low<=high) {
             int mid = low + (high - low) / 2;
-            if(arr[mid] <= target) {
+            if(arr.get(mid) <= target) {
                 ans = mid;
                 low = mid + 1;
             }

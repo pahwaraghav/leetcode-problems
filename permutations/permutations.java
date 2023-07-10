f534cbe02e3b4ca4c7fc13fe91936f1ab1ace4da
Raghav Pahwa <72634414+pahwaraghav@users.noreply.github.com>
2023-07-10 21:34:48 +0530

    Time: 1 ms (98.91%) | Memory: 44.2 MB (27.24%) - LeetSync

--- permutations/permutations.java
+++ permutations/permutations.java
@@ -1,26 +1,24 @@
 class Solution {
+    int min = Integer.MIN_VALUE;
     public List<List<Integer>> permute(int[] nums) {
-        HashMap<Integer, Integer> hmap = new HashMap<>();
-        for(int num: nums){
-            hmap.put(num, hmap.getOrDefault(num,0)+1);
-        }
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> curr = new ArrayList<>();
-        helper(nums, hmap, curr, ans);
+        helper(nums, curr, ans);
         return ans;
     }
-    public void helper(int[] nums, HashMap<Integer, Integer> hmap, List<Integer> curr, List<List<Integer>> ans){
+    public void helper(int[] nums, List<Integer> curr, List<List<Integer>> ans){
         if(curr.size() == nums.length){
             ans.add(new ArrayList(curr));
             return;
         }
-        for(Map.Entry<Integer,Integer> e: hmap.entrySet()){
-            if(e.getValue() == 0) continue;
-            curr.add(e.getKey());
-            hmap.put(e.getKey(), e.getValue()-1);
-            helper(nums, hmap, curr, ans);
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == min) continue;
+            curr.add(nums[i]);
+            int temp = nums[i];
+            nums[i] = min;
+            helper(nums, curr, ans);
             curr.remove(curr.size()-1);
-            hmap.put(e.getKey(), e.getValue()+1);
+            nums[i] = temp;
         }
     }
 }

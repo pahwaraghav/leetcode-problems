484fdf750f5d511ffb10a8b8bb8eb857b7fbe2a9
Raghav Pahwa <72634414+pahwaraghav@users.noreply.github.com>
2023-07-13 19:13:29 +0530

    Time: 0 ms (100.00%) | Memory: 39.2 MB (71.67%) - LeetSync

--- climbing-stairs/climbing-stairs.java
+++ climbing-stairs/climbing-stairs.java
@@ -1,12 +1,13 @@
 class Solution {
     public int climbStairs(int n) {
         if(n==1) return 1;
-        int[] mem = new int[n+1];
-        mem[1] = 1;
-        mem[2] = 2;
+        int a = 1;
+        int b = 2;
         for(int i=3; i<=n; i++) {
-            mem[i] = mem[i-1] + mem[i-2];
+            int c = a + b;
+            a = b;
+            b = c;
         }
-        return mem[n];
+        return b;
     }
 }

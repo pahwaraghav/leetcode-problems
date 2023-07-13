c99eba8522f82805a92d963d8e6897522cfcacf6
Raghav Pahwa <72634414+pahwaraghav@users.noreply.github.com>
2023-07-14 01:34:07 +0530

    Time: 66 ms (11.79%) | Memory: 46 MB (5.43%) - LeetSync

--- coin-change/coin-change.java
+++ coin-change/coin-change.java
@@ -1,16 +1,21 @@
 class Solution {
+    long max = Integer.MAX_VALUE;
     public int coinChange(int[] coins, int amount) {
-        int[] res = new int[amount+1];        
-        for(int i=0; i<=amount;i++) res[i] = Integer.MAX_VALUE;
-        res[0] = 0;
-        for(int coin: coins){
-            for(int i=coin; i<=amount;i++){
-                if(res[i-coin] == Integer.MAX_VALUE) continue;
-                res[i] = Math.min(res[i], res[i-coin]+1);
-            }
-        }
-        int ans = res[amount];
-        if(ans==Integer.MAX_VALUE) return -1;
-        return ans;
+        Arrays.sort(coins);
+        System.out.println(Arrays.toString(coins));
+        long[][] dp = new long[amount+1][coins.length+1];
+        long ans = helper(coins, amount, coins.length-1, dp);
+        if(ans >= max) return -1;
+        else return (int) ans;
+    }
+    public long helper(int[] coins, int amount, int i, long[][] dp) {
+        if(i<0) return max;
+        if(amount == 0) return 0;
+        //System.out.println(i + " " + amount);
+        if(dp[amount][i] != 0) return dp[amount][i];
+        long take = max, notTake = max;
+        if(coins[i] <= amount) take = 1 + helper(coins, amount-coins[i], i, dp);
+        notTake = helper(coins, amount, i-1, dp);
+        return dp[amount][i] = Math.min(take, notTake);
     }
 }

8709e1b03267552ff4356008f5ddf4c621798605
Raghav Pahwa <72634414+pahwaraghav@users.noreply.github.com>
2023-07-14 01:35:02 +0530

    Time: 66 ms (11.78%) | Memory: 46 MB (5.43%) - LeetSync

--- coin-change/coin-change.java
+++ coin-change/coin-change.java
@@ -2,6 +2,7 @@ class Solution {
     long max = Integer.MAX_VALUE;
     public int coinChange(int[] coins, int amount) {
         Arrays.sort(coins);
+        System.out.println(Arrays.toString(coins));
         long[][] dp = new long[amount+1][coins.length+1];
         long ans = helper(coins, amount, coins.length-1, dp);
         if(ans >= max) return -1;
@@ -10,6 +11,7 @@ class Solution {
     public long helper(int[] coins, int amount, int i, long[][] dp) {
         if(i<0) return max;
         if(amount == 0) return 0;
+        //System.out.println(i + " " + amount);
         if(dp[amount][i] != 0) return dp[amount][i];
         long take = max, notTake = max;
         if(coins[i] <= amount) take = 1 + helper(coins, amount-coins[i], i, dp);

26d8a6fc2886eb2b0bb4470cba0009e03a33dce6
Raghav Pahwa <72634414+pahwaraghav@users.noreply.github.com>
2023-08-02 17:47:22 +0530

    Time: 1 ms (98.58%) | Memory: 43.6 MB (90.84%) - LeetSync

---
+++ 46-permutations/permutations.java
@@ -0,0 +1,24 @@
+class Solution {
+    int min = Integer.MIN_VALUE;
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        helper(nums, new ArrayList<Integer>(), ans);
+        return ans;
+    }
+    public void helper(int[] nums, List<Integer> curr, List<List<Integer>> ans){
+        if(curr.size() == nums.length){
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == min) continue;
+            curr.add(nums[i]);
+            int temp = nums[i];
+            nums[i] = min;
+            helper(nums, curr, ans);
+            curr.remove(curr.size()-1);
+            nums[i] = temp;
+        }
+    }
+}
+
